0f137090db1a200b701ea58d2bb93a8a97d4e67a
jamestucker1988 <xat2212@gmail.com>
2020-12-10 18:13:12 +0530

    add file named file.rb

---
+++ bin/file.rb
@@ -0,0 +1,9 @@
+# rubocop:disable Metrics/ModuleLength, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Style/For
+
+
+
+
+
+
+
+# rubocop:enable Metrics/ModuleLength, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Style/For
