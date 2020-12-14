8db7c1ba29602d675505fcb53e6e58da49eb4e72
jamestucker1988 <xat2212@gmail.com>
2020-12-14 13:00:10 +0530

    include module colorize in white_space.rb

--- lib/white_space.rb
+++ lib/white_space.rb
@@ -1,12 +1,12 @@
 require_relative('./file')
 require 'colorize'
 module White_space
-  def trail_space(func) 
-    func.each_with_index do |x,i| 
+  def trail_space(func)
+    func.each_with_index do |x, i|
       if i < (func.size - 1)
-        puts "trailing white_space at  line #{i+1} ".yellow if x.match?(/\s+\n/)
+        puts "trailing white_space at  line #{i + 1} ".yellow if x.match?(/\s+\n/)
       else
-        puts "trailing white_space at  line #{i+1} ".yellow  if x.match?(/\s+$/)
+        puts "trailing white_space at  line #{i + 1} ".yellow if x.match?(/\s+$/)
       end
     end
   end
