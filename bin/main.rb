a9aaabd69d3e2a9bc3628fce227f8ff7542b1951
jamestucker1988 <xat2212@gmail.com>
2020-12-14 21:32:32 +0530

    call module tag and remove repeat line

--- bin/main.rb
+++ bin/main.rb
@@ -1,18 +1,17 @@
 require_relative '../lib/file' 
 require_relative('../lib/brackets')
 require_relative('../lib/white_space')
+require_relative('../lib/tag')
 require_relative('../lib/indentation')
-
-
 require 'colorize'
 class Linter
   extend White_space
   extend Indentation 
-  extend Brackets
+  extend Brackets 
+  extend Tag
   File_read.new('bin/bug.rb')
 end
 Linter.trail_space(File_read.p_file('bin/bug.rb'))
 Linter.indent(File_read.p_file('bin/bug.rb'))
-Linter.brack(file.readlines('bin/bug.rb'))
-
+Linter.brack(File.readlines('bin/bug.rb'))
 # rubocop:enable Metrics/ModuleLength, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Style/For
