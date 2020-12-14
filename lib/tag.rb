25f61136252ffc074219d80387b7d06d85bb2c5c
jamestucker1988 <xat2212@gmail.com>
2020-12-14 23:00:25 +0530

    refactor code in tag.rb

--- lib/tag.rb
+++ lib/tag.rb
@@ -1,17 +1,19 @@
 require_relative('./file') 
 module Tag
 def brack(func)
-  brack_pair = {'{'=>'}','['=>']','('=>')'} 
-  arr = [] 
-  arr1= []
   func.each_with_index do |line,i| 
-    #  line_r = line.split(' ') 
-    # puts line
 unless line.empty?
-    reg1 = /.*[)].*/
-    reg = /.*[(].*[)].*/
-    puts "missing-tag ( at line #{i += 1}" if !reg.match?(line) && reg1.match?(line)
-
+    reg1 = /.*[)|\]].*/
+    reg = /.*[(|\[].*[)|\]].*/
+    reg2 = /.*[}].*/ 
+    reg3 = /.*({).*}.*/ 
+    # reg4 =  /.*[(|\[].*/ 
+    reg5 = /.*[{].*/ 
+    reg6 = /.*(}).*}.*/ 
+    puts "missing-token  at line #{i += 1}".red if !reg.match?(line) && reg1.match?(line)
+    puts "missing-token  at line #{i += 1}".red if !reg3.match?(line) && reg2.match?(line) 
+    puts "missing-token  at line #{i += 1}".red if !reg3.match?(line) && reg2.match?(line)
+    puts "missing-token  at line #{i += 1}".red if !reg6.match?(line) && reg5.match?(line)
 end
 end
 end
