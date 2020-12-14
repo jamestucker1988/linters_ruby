39fc9770d8e7fbd5fca8cdcab50ee9b56171fe87
jamestucker1988 <xat2212@gmail.com>
2020-12-14 13:25:43 +0530

    modify main.rb in response to brackets.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,13 +1,18 @@
-require_relative '../lib/file'
-require_relative('../lib/indentation')
+require_relative '../lib/file' 
+require_relative('../lib/brackets')
 require_relative('../lib/white_space')
+require_relative('../lib/indentation')
+
+
 require 'colorize'
 class Linter
   extend White_space
-  extend Indentation
+  extend Indentation 
+  extend Brackets
   File_read.new('bin/bug.rb')
 end
 Linter.trail_space(File_read.p_file('bin/bug.rb'))
 Linter.indent(File_read.p_file('bin/bug.rb'))
+Linter.brack(file.readlines('bin/bug.rb'))
 
 # rubocop:enable Metrics/ModuleLength, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Style/For
