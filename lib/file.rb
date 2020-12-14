ee0cb8329916652de9c83a9ea97a1de02d7e1897
jamestucker1988 <xat2212@gmail.com>
2020-12-14 12:07:52 +0530

    modift to read line in the file and check path and name

--- lib/file.rb
+++ lib/file.rb
@@ -1,11 +1,13 @@
-class File_read 
+# require_relative('./white_space')
+# include White_space
+class File_read
   attr_accessor :file_name
   def initialize(file_name)
-    @file_name = file_name 
-    puts "check filepath or name"  unless (File.exist?(file_name) || Dir.exist?(file_name))
-  end 
-  def p_file 
-    file_read = File.open "#{@file_name}" 
-     file_read.each {|x| puts x}
+    @file_name = file_name
+    puts 'check filepath or name' unless File.exist?(file_name) || Dir.exist?(file_name)
   end
-end 
+
+  def self.p_file(file_name)
+    File.readlines file_name
+  end
+end
