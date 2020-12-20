55b2193fd0de4408b49fca49f6dbd31e2dc48448
jamestucker1988 <xat2212@gmail.com>
2020-12-20 17:52:30 +0530

    remodel code

--- lib/indentation.rb
+++ lib/indentation.rb
@@ -1,41 +1,42 @@
 require_relative('./file')
-module Indentation
-  def indent(func)
-    puts 'two space of indent to escape indent error'.yellow
+module Indentation 
+  def indent(func)     
+    $arr,$arr1 = [],[]
+    puts 'two space of indent to escape indent error'.yellow  
+    cur_indent,line1_space, line2_space= 0,0,0
+    res_key(func)
+    $i = 0
+    until   (func.size) == $i
+      $i += 1  until !func[$i].empty?
+     obs_indent = (line2_space - line1_space)
+      if   obs_indent !=  cur_indent
+        puts "indentation error at line  #{$i} #{cur_indent} #{line1_space},#{line2_space} " .red 
+      end 
+       line1_space,line2_space = 0,0
+      func[$i-1].each_char{|c| c ==' ' ? line1_space +=1 : break}
+      func[$i].each_char{|c| c ==' ' ? line2_space +=1 : break} unless func.size  - 1 == $i 
+      cur_indent = indent_c(func)
+     $i += 1
+    end
+  end 
+  def indent_c(func)
+    arr,arr2 = %w(def class if unless until case while begin module do),%w(else end elsif) 
+  if arr.include?($arr1[$i-1]) || (arr2.include?($arr1[$i-1]) && $arr1[$i-1] != 'end')
+    cur_indent  = 2 
+  elsif arr2.include?($arr1[$i]) && !arr.include?($arr1[$i-1])
+    cur_indent = -2  
+  else 
     cur_indent = 0
-    c = 1
-    $i = 1
-    line1_space = 0
-    line2_space = 0
-    until func.size < 2
-      first_line_res_word = func[0].split(' ')
-      last_line_res_word = func[1].split(' ')
-      func[0].each_char { |c| c == ' ' ? line1_space += 1 : break }
-      func[1].each_char { |c| c == ' ' ? line2_space += 1 : break }
-      cur_indent = indent_expected(line1_space, line2_space, cur_indent, first_line_res_word, last_line_res_word)
-      func.slice!(0, 1)
-      observed_indent = (line2_space - line1_space).abs
-      if observed_indent != cur_indent
-        puts "indentation error at line  #{$i += 1}" .red
-        c = 0
+  end 
+  cur_indent
+end
+  def res_key(func)
+    $i = 0
+      until   (func.size) == $i
+        res_word1 = func[$i]    
+        c = (res_word1.match( /(\s+do\s+)/) ||  res_word1 .match( /(\s?+\w+\s?)/)) .to_s.strip 
+     c ? $arr1 << c :   $arr1 << ' '
+   $i += 1
       end
-    end
-    puts 'no indentation error in file'.green unless c.zero?
-  end
-  private
-  def indent_expected(_line1_space, _line2_space, cur_indent, first_line_res_word, last_line_res_word)
-    keyword = %w[def class if unless do case while begin]
-    if keyword.include?(first_line_res_word.first) && (first_line_res_word.first != first_line_res_word.last)
-      cur_indent += 4
-    elsif  !keyword.include?(first_line_res_word.first) && !keyword.include?(first_line_res_word.last) && last_line_res_word.first == 'end' && cur_indent.positive?
-      cur_indent -= 4
-    elsif  first_line_res_word[-1] == 'do' || first_line_res_word[-2] == 'do'
-      cur_indent += 4
-    elsif first_line_res_word.first == first_line_res_word.last && first_line_res_word.first == 'elsif'
-      cur_indent += 4
-    elsif keyword.include?(first_line_res_word.first) && (first_line_res_word.first != first_line_res_word.last) && cur_indent.positive?
-      cur_indent -= 4
-    end
-    cur_indent
-  end
+  end 
 end
