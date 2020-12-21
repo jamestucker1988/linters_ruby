40eb8901b9d4821857eefbb13a1010729d2b80fe
jamestucker1988 <xat2212@gmail.com>
2020-12-21 22:48:37 +0530

    modify main.rb in viewof end_count.rb

--- bin/main.rb
+++ bin/main.rb
@@ -1,18 +1,25 @@
 require_relative '../lib/file' 
 require_relative('../lib/brackets')
 require_relative('../lib/white_space')
-require_relative('../lib/tag')
+require_relative('../lib/tag') 
+require_relative('../lib/test')
+require_relative('../lib/end_count')
 require_relative('../lib/indentation')
 require 'colorize'
 class Linter
   extend White_space
   extend Indentation 
   extend Brackets 
-  extend Tag
+  extend Tag 
+  extend End_count 
+  extend Test
   File_read.new('bin/bug.rb')
 end
-Linter.trail_space(File_read.p_file('bin/bug.rb'))
-Linter.indent(File_read.p_file('bin/bug.rb'))
+# Linter.trail_space(File_read.p_file('bin/bug.rb'))
+Linter.indent(File_read.p_file('bin/bug.rb')) 
+# Linter.try(File_read.p_file('bin/bug.rb'))
 Linter.brack(File.readlines('bin/bug.rb'))
 Linter.bracket(File.readlines('bin/bug.rb'))
+key_word = Linter.end_num 
+Linter.end_number(key_word)
 # rubocop:enable Metrics/ModuleLength, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Style/For
