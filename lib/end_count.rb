97c5f9c68ed62950e71b46c68852ead9483580d9
jamestucker1988 <xat2212@gmail.com>
2020-12-21 22:35:15 +0530

    add file to report unexpected end

---
+++ lib/end_count.rb
@@ -0,0 +1,16 @@
+require_relative('./file') 
+module End_count 
+  def end_number(keyword) 
+    arr =  %w(def class if unless until case while begin module do)
+   count = 0 
+   key_count = 0 
+   keyword.each do |key| 
+    if key == 'end' 
+      count += 1 
+    elsif  arr.include?(key)
+      key_count += 1
+    end
+   end 
+  puts "unexpected end-of-input expecting end".blue if (key_count != count)
+  end
+end
