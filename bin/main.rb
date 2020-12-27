742d72eb08ebf4ea0e25c51e770096f7f6a2cc6f
jamestucker1988 <xat2212@gmail.com>
2020-12-27 13:38:22 +0530

    modify main file to include other modules instead of extend

--- bin/main.rb
+++ bin/main.rb
@@ -1,25 +1,24 @@
-require_relative '../lib/file' 
+require_relative '../lib/file'
 require_relative('../lib/brackets')
 require_relative('../lib/white_space')
-require_relative('../lib/tag') 
 require_relative('../lib/test')
 require_relative('../lib/end_count')
+require_relative('../lib/empty_line')
 require_relative('../lib/indentation')
 require 'colorize'
 class Linter
-  extend White_space
-  extend Indentation 
-  extend Brackets 
-  extend Tag 
-  extend End_count 
-  extend Test
-  File_read.new('bin/bug.rb')
-end
-# Linter.trail_space(File_read.p_file('bin/bug.rb'))
-Linter.indent(File_read.p_file('bin/bug.rb')) 
-# Linter.try(File_read.p_file('bin/bug.rb'))
-Linter.brack(File.readlines('bin/bug.rb'))
-Linter.bracket(File.readlines('bin/bug.rb'))
-key_word = Linter.end_num 
-Linter.end_number(key_word)
+  include White_space
+  include Indentation
+  include Brackets
+  include End_count1
+  include Test
+  include Empty_line1
+  # File_read.new('../lib/bug.rb')
+end 
+lint = Linter.new
+lint.indent(File.readlines('lib/bug.rb'))
+lint.bracket(File.readlines('lib/bug.rb'))
+key_word = lint.end_num
+lint.end_number(key_word)
+lint.empty_line1(File.readlines('lib/bug.rb'))
 # rubocop:enable Metrics/ModuleLength, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Style/For
