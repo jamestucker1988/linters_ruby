39c0bcafdd0d0b958791e2b3e2a72f640b57f329
jamestucker1988 <xat2212@gmail.com>
2020-12-27 14:34:36 +0530

    change class method to instance methods

--- lib/indentation.rb
+++ lib/indentation.rb
@@ -1,44 +1,53 @@
+# lib/indentation.rb
 require_relative('./file')
-module Indentation 
-  def indent(func)     
-    $arr,$arr1 = [],[]
-    puts 'two space of indent to escape indent error'.yellow  
-    cur_indent,line1_space, line2_space= 0,0,0
+module Indentation
+  def indent(func)
+    $arr = []
+    $arr1 = []
+    puts 'two space of indent to escape indent error'.yellow
+    cur_indent = 0
+    line1_space = 0
+    line2_space = 0
     res_key(func)
     $i = 0
-    until   (func.size) == $i
-      $i += 1  until !func[$i].empty?
-     obs_indent = (line2_space - line1_space)
-      if   obs_indent !=  cur_indent
-        puts "indentation error at line  #{$i} #{cur_indent} #{line1_space},#{line2_space} " .red 
-      end 
-       line1_space,line2_space = 0,0
-      func[$i-1].each_char{|c| c ==' ' ? line1_space +=1 : break}
-      func[$i].each_char{|c| c ==' ' ? line2_space +=1 : break} unless func.size  - 1 == $i 
-      cur_indent = indent_c(func)
-     $i += 1
+    until   func.size == $i
+      $i += 1 while func[$i].empty?
+      obs_indent = (line2_space - line1_space)
+      if obs_indent != cur_indent
+        puts "indentation error at line  #{$i} #{cur_indent} #{line1_space},#{line2_space} " .red
+      end
+      line1_space = 0
+      line2_space = 0
+      func[$i - 1].each_char { |c| c == ' ' ? line1_space += 1 : break }
+      func[$i].each_char { |c| c == ' ' ? line2_space += 1 : break } unless func.size - 1 == $i
+      cur_indent = indent_c
+      $i += 1
     end
-  end 
-  def indent_c(func)
-    arr,arr2 = %w(def class if unless until case while begin module do),%w(else end elsif) 
-  if arr.include?($arr1[$i-1]) || (arr2.include?($arr1[$i-1]) && $arr1[$i-1] != 'end')
-    cur_indent  = 2 
-  elsif arr2.include?($arr1[$i]) && !arr.include?($arr1[$i-1])
-    cur_indent = -2  
-  else 
-    cur_indent = 0
-  end 
-  cur_indent
+  end
+
+  def indent_c
+    arr = %w[def class if unless until case while begin module do]
+    arr2 = %w[else end elsif]
+    if arr.include?($arr1[$i-1]) || (arr2.include?($arr1[$i-1]) && $arr1[$i-1] != 'end')
+      cur_indent = 2
+    elsif arr2.include?($arr1[$i]) && !arr.include?($arr1[$i - 1])
+      cur_indent = -2
+    else
+      cur_indent = 0
+    end
+    cur_indent
 end
+
   def res_key(func)
     $i = 0
-      until   (func.size) == $i
-        res_word1 = func[$i]    
-        c = (res_word1.match( /(\s+do\s+)/) ||  res_word1 .match( /(\s?+\w+\s?)/)) .to_s.strip 
-     c ? $arr1 << c :   $arr1 << ' '
-   $i += 1
-      end
-  end 
+    until func.size == $i
+      res_word1 = func[$i]
+      c = (res_word1.match(/(\s+do\s+)/) || res_word1 .match(/(\s?+\w+\s?)/)) .to_s.strip
+      $arr1 << (c ? c : ' ')
+      $i += 1
+    end
+  end
+
   def end_num
     $arr1
   end
