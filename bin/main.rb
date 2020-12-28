e421e1892ac0b11ff51be8dc5b4f03a0b975051d
jamestucker1988 <xat2212@gmail.com>
2020-12-28 14:59:28 +0530

    add a method to ask file name

--- bin/main.rb
+++ bin/main.rb
@@ -6,15 +6,21 @@ require_relative('../lib/empty_line')
 require_relative('../lib/indentation')
 # class
 class Linter
+  attr_accessor :file_name
   include White_space
   include Indentation
   include Brackets
   include End_count1
-  include Empty_line1
+  include Empty_line1 
+  def input_file_name 
+    puts "enter file_path or name"
+    @file_name =  gets.chomp
+  end
 end
-lint = Linter.new
-lint.indent(File.readlines('lib/bug.rb'))
-lint.bracket(File.readlines('lib/bug.rb'))
+lint = Linter.new 
+file_name = lint.input_file_name
+lint.indent(File.readlines("#{file_name}"))
+lint.bracket(File.readlines("#{file_name}"))
 key_word = lint.end_num
 lint.end_number(key_word)
-lint.empty_line1(File.readlines('lib/bug.rb'))
+lint.empty_line1(File.readlines("#{file_name}"))
