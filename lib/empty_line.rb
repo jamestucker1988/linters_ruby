f43c51340351c8f7394fc6b74cbc0f1cbfc800c5
jamestucker1988 <xat2212@gmail.com>
2021-01-03 14:58:28 +0530

    modify code to escape 1 empty line

--- lib/empty_line.rb
+++ lib/empty_line.rb
@@ -1,8 +1,12 @@
 module Empty_line1
   def empty_line1(func)
+    c = 0
     func.each_with_index do |line, i|
-      puts "empty line at #{i + 1}" if line.strip.empty?
+      c += 1 if line.strip.empty?
+      if c > 1
+        puts "empty line at #{i + 1}"
+        c = 0
+      end
+    end
     end
   end
-end
-# rubocop:enable Metrics/ModuleLength, Metrics/MethodLength, Metrics/CyclomaticComplexity, Metrics/PerceivedComplexity, Style/For
