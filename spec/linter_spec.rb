80409da8b7f7131737d4967772c78804a3775cc8
jamestucker1988 <xat2212@gmail.com>
2021-01-07 09:35:27 +0530

    modify linter_spec.rb

--- spec/linter_spec.rb
+++ spec/linter_spec.rb
@@ -1,76 +1,68 @@
-# require('./bin/main')
-  require('./bin/main')
-  RSpec.describe Linter do
-    let(:lint) { Linter.new('bug.rb') }
-    let(:file_attr) { File.readlines('bug.rb') }
-    context 'when indentation file is called' do
-      describe '#indent' do 
-        it { is_expected.to respond_to(:send).with(1).argument }
-        context 'when $i is less than func.size ' do
-          before(:each) do 
-            $i = 0
-            indent = double
-            allow(indent).to receive(:indent_c)
-            allow(lint).to receive(:puts) { "indentation error at line  #{$i} #{lint.indent.cur_indent} #{lint.indent.line1_space},#{lint.indent.line2_space} " }
-          end
-        end
-      end
-      describe '#res_key' do
-        it { expect($arr1).to be_an(Array) }
-      end
-    end
-    context 'when empty_line file is called' do
-      describe '#empty_line1' do
-        it { is_expected.to respond_to(:send).with(1).argument }
-        it { expect { lint.send(:empty_line1, file_attr).to yield_control } }
-      end
-    end
-    context 'when white_space file is called' do
-      describe '#trail_space' do
-        it { is_expected.to respond_to(:send).with(1).argument }
-      end
-    end
-    context 'when end_count is called' do
-      describe '#end_number' do
-        it { is_expected.to respond_to(:send).with(1).argument }
-        specify { expect { lint.send(:end_number, file_attr).func.to yield_control } }
-      end
-    end
-
-    describe '#indent_c' do
-      it 'should return a integer' do 
-        allow(lint).to receive(:indent_c).and_return(1)
-        expect(lint.indent_c).to be_between(-2, 2).inclusive
-      end
+require('./bin/main')
+RSpec.describe Linter do
+  let(:lint) { Linter.new('bug.rb') }
+  let(:file_attr) { File.readlines('bug.rb') }
+  let(:arr) { %w(( )) }
+  context 'when indentation file is called' do
+    describe '#indent' do
+      it { is_expected.to respond_to(:indent_calc).with(2).argument }
+      it { is_expected.to respond_to(:res_key) }
     end
     describe '#res_key' do
-      it { is_expected.to respond_to(:send).with(1).argument }
-      it 'should have a initialized constant $i' do
-        expect($i).to be_an(Numeric)
-      end
+      it { expect(lint.res_key).to be_an(Array) }
     end
-    describe '#end_num' do
-      it 'should return a array' do
-        expect($arr1).to be_an(Array)
-      end
+    describe '#indent_call' do
+      it { expect { raise 'indent_call' }.to raise_error(RuntimeError) }
     end
-    #describe Brackets do
-      describe '#bracket' do
-        it { is_expected.to respond_to(:send).with(1).argument }
-        specify { expect { print('this missing bracket') }.to output.to_stdout }
-      end
-    #end
-    #describe End_count1 do
-      describe '#end_number' do
-        it { is_expected.to respond_to(:send).with(1).argument }
-        specify { expect { print('unexpected end-of-input expecting end') }.to output.to_stdout }
-      end
-    #  describe Empty_line1 do
-        describe '#empty_line1' do
-          it { is_expected.to respond_to(:send).with(1).argument }
-          specify { expect { print('empty line at #{i + 1}') }.to output.to_stdout }
-        end
-      #end
-    #end
-  #end
+    describe '#indent_calc' do
+      it { is_expected.to respond_to(:indent_c).with(2).argument }
+      it { is_expected.to respond_to(:line_cal).with(1).argument }
+      it { is_expected.to respond_to(:indent_call).with(1).argument }
+      it { expect { raise 'indent_calc' }.to raise_error(RuntimeError) }
+    end
+    describe '#line_cal' do
+      it { expect(lint.line_cal(3)).to be_an(Integer) }
+    end
+    describe '#arrin?' do
+      it { expect(lint.arrin?(arr, 2)).to be_nil }
+      it { expect(lint.arrin?(['def'], 1)).to be_truthy }
+    end
+    describe '#arr2in?' do
+      it { expect(lint.arr2in?(['else'], 24)).to be_nil }
+    end
+    describe '#indent_c' do
+      it { expect(lint.indent_c(['def'], 1)).to be_truthy }
+    end
   end
+  context 'when trail_space is called' do
+    describe '#trail_space' do
+      it { expect { raise 'trail_space' }.to raise_error(RuntimeError) }
+    end
+  end
+  context 'when end_number is called' do
+    describe '#end_number' do
+      it { expect { raise 'expecting end' }.to raise_error(RuntimeError) }
+    end
+  end
+  context 'when empty_line1 is called' do
+    describe '#empty_line1' do
+      it { expect { raise 'empty_line1' }.to raise_error(RuntimeError) }
+    end
+  end
+  context 'when bracket is called' do
+    describe '#bracket' do
+      it { is_expected.to respond_to(:brack_block).with(3).argument }
+    end
+    describe 'brack_block' do
+      it { is_expected.to respond_to(:r_brack).with(2).argument }
+    end
+    describe 'r_brack' do
+      it { expect { raise('r_brack') }.to raise_error(RuntimeError) }
+    end
+  end
+  context 'when f_edit is called' do
+    describe '#f_edit' do
+      it { expect(lint.f_edit).to be_an(Array) }
+    end
+  end
+end
